b6e883af921dc628d6b7d51acf4b6a00860c45e6
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-18 02:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	for(let i=0; i<str.length; i++){
+		let fc=str.indexOf(str.charAt(i));
+		let lc=str.lastIndexOf(str.charAt(i));
+		if(fc==lc){
+			return str.charAt(i);
+		}
+	}
+	return "null";
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
